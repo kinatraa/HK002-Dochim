4c61258809c1360cffa4f23d0a462295d02c4f27
kinatraa <hakien.237@gmail.com>
2025-01-15 21:55:14 +0700

    add: new tilemap

--- Assets/Project/Scripts/Modules/GamePlay/DiamondClick.cs
+++ Assets/Project/Scripts/Modules/GamePlay/DiamondClick.cs
@@ -2,11 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Tilemaps;
 
-public class DiamondClick : MonoBehaviour, IPointerClickHandler
+public class DiamondClick : MonoBehaviour
 {
-    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    public Tilemap Tilemap;
+
+    private Camera _camera;
+
+    void Start()
     {
-        Destroy(gameObject);
+        _camera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 worldPoint = _camera.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int gridPosition = Tilemap.WorldToCell(worldPoint);
+            if (Tilemap.GetTile(gridPosition) != null)
+            {
+                Tilemap.SetTile(gridPosition, null);
+                DropTile();
+            }
+        }
+    }
+
+    private void DropTile()
+    {
+        BoundsInt bounds = Tilemap.cellBounds;
+
+        for (int y = bounds.yMin; y < bounds.yMax; y++)
+        {
+            for (int x = bounds.xMin; x < bounds.xMax; x++)
+            {
+                if (Tilemap.GetTile(new Vector3Int(x, y, 0)) != null) continue;
+                Queue<Vector3Int> aboveTiles = new Queue<Vector3Int>();
+
+                GetAboveTiles(aboveTiles, x, y);
+                DropAboveTiles(aboveTiles, x, y);
+            }
+        }
+    }
+
+    private void GetAboveTiles(Queue<Vector3Int> aboveTiles, int x, int y)
+    {
+        BoundsInt bounds = Tilemap.cellBounds;
+
+        for (int k = y + 1; k < bounds.yMax; k++)
+        {
+            if (Tilemap.GetTile(new Vector3Int(x, k, 0)) == null) continue;
+
+            aboveTiles.Enqueue(new Vector3Int(x, k, 0));
+        }
+    }
+
+    private void DropAboveTiles(Queue<Vector3Int> aboveTiles, int x, int y)
+    {
+        while (aboveTiles.Count > 0)
+        {
+            Vector3Int tile = aboveTiles.Dequeue();
+            Tilemap.SetTile(new Vector3Int(x, y++, 0), Tilemap.GetTile(tile));
+            Tilemap.SetTile(new Vector3Int(x, y, 0), null);
+        }
     }
 }
--- Assets/Project/Scripts/Modules/GamePlay/DiamondManager.cs
+++ Assets/Project/Scripts/Modules/GamePlay/DiamondManager.cs
@@ -2,32 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Tilemaps;
 
 public class DiamondManager : MonoBehaviour
 {
-    public GameObject[] DPrefabs;
-    public Transform Board;
+    public Tilemap Tilemap;
+    public TileBase[] DTiles;
     public int Rows = 8;
     public int Columns = 8;
 
-    private GameObject[,] _diamonds;
-
     void Start()
     {
-        _diamonds = new GameObject[Rows, Columns];
         GenerateBoard();
     }
 
     private void GenerateBoard()
     {
-        for (int i = 0; i < Rows; i++)
+        for (int i = -Rows / 2; i < Rows / 2; i++)
         {
-            for (int j = 0; j < Columns; j++)
+            for (int j = -Columns / 2; j < Columns / 2; j++)
             {
-                int k = Random.Range(0, DPrefabs.Length);
-
-                GameObject diamond = Instantiate(DPrefabs[k], Board);
-                _diamonds[i, j] = diamond;
+                Tilemap.SetTile(new Vector3Int(i, j, 0), DTiles[Random.Range(0, DTiles.Length)]);
             }
         }
     }
